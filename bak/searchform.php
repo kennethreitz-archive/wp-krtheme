06bffa52fd940c766347000bfc413d8ef5903f02
Kenneth Reitz <me@kennethreitz.com>
2010-09-22 16:03:53 -0400

    Removed backup.

--- bak/searchform.php
+++
@@ -1,7 +0,0 @@
-<form method="get" action="<?php bloginfo('url'); ?>" class="sitesearch">
-	<fieldset>
-		<legend>Search for this site</legend>
-		<input type="text" id="s" name="s" value="<?php the_search_query(); ?>" placeholder="Search Keyword" />
-		<input type="submit" id="searchsubmit" value="Search" />
-	</fieldset>
-</form>
